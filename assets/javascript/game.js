831bdd59bbe2ed9856477c8bf7837c0ff0ce6cde
Molly Graham <mollyclaire.graham@gmail.com>
2019-10-15 12:10:30 -0500

    Added some functionality to the start function. The game now prints a random word to the console and pushes dashes into an empty array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,53 +20,35 @@ var guessesRemaining = 10;
 
 // FUNCTIONS
 //------------------------------------------------------------------------
-// Computer randomly selects one word
-var randomGuess = artistList[Math.floor(Math.random() * artistList.length)];
-console.log(randomGuess);
+
+function start() {
+    // Resets the guesses back to 0
+    guessesRemaining = 10;
+
+    // A word is chosen randomly from the artistList array
+   chosenWord = artistList[Math.floor(Math.random() * artistList.length)];
+   console.log(chosenWord);
+
+    // The word is broken down into letters
+   lettersinChosenWord = chosenWord.split(""); 
+
+   // The .length property here will tell us how many letters/blanks are in the word
+   numBlanks = lettersinChosenWord.length; 
+
+   // Reset both arrays from potential previous round
+   dashesAndLetters = [];
+   wrongGuesses = [];
+
+  for (var i = 0; i < numBlanks; i++) {
+    dashesAndLetters.push("_");
+  }
+}
+start() 
+
 
 // MAIN PROCESSES
 //------------------------------------------------------------------------
 
 
-// Declare variables for text that will be replaced within HTML
-var winsText = document.getElementById("winsText");
-var lettersGuessedText = document.getElementById("lettersGuessedText");
-var numbersGuessedText = document.getElementById("numbersGuessedText");
 
-// Dashes appear on the page based on the number of letters in the chosen word
-var dashes = function createDashes() {
-    for (i = 0; i < randomGuess.length; i++) {
-        dash.push("-");
-    }
-    return dash; // Why do I need to return here?
-}
-console.log(dashes());
-
-// User guesses and presses any letter key to start the game
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-// Searches for a correct letter within a random word (chosen by the computer)
-    var correctLetter = artistList.indexOf(randomGuess);
-// If the letter is in the word, a dash is replaced with the letter that was guessed 
-        if (correctLetter != -1) {
-        dash.push(correctLetter);
-        } else { // Else, the letter appears in the "Letters Already Guessed" list
-        guessedLetters.push(userGuess);
-        }
-        console.log(correctLetter);
-
-// The number of guesses remaining decreases one number
-
-}
-
-// This function governs what happens when the user makes an incorrect guess (that they haven't guessed before)
-function updateGuesses(letter) {
-    if (guessedLetters.indexOf(letter) === -1 && lettersInWord.indexOf(letter) === -1) {
-        this.guessLetters.push(letter);
-        this.guessesRemaining--;
-
-        document.querySelector("numbersGuessedText").innerHTML = guessesRemaining;
-        document.querySelector("lettersGuessedText").innerHTML = guessedLetters.join(", ");
-    }
-}
 
